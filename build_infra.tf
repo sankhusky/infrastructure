3f7973571ce89a087520d14a22e78fceed061906
Sanket Pimple <pimple.s@northeastern.edu>
2020-10-18 15:45:57 -0400

    Added security grps, s3 and rds

--- build_infra.tf
+++ build_infra.tf
@@ -4,13 +4,13 @@ variable "ssh_key_name" {
   default = "csye6225"
 }
 
-variable "access_key" {
-  type = string
-}
+# variable "access_key" {
+#   type = string
+# }
 
-variable "secret_key" {
-  type = string
-}
+# variable "secret_key" {
+#   type = string
+# }
 variable "region" {
   type = string
 }
@@ -36,15 +36,15 @@ variable "availability_zone2" {
 variable "availability_zone3" {
   type = string
 }
-variable "route_destination_cidr"{
-  type=string
+variable "route_destination_cidr" {
+  type = string
 }
 
 provider "aws" {
   # region = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region     = var.region
+  # access_key = var.access_key
+  # secret_key = var.secret_key
+  region = var.region
 }
 
 variable "availabilityZone" {
@@ -137,6 +137,168 @@ resource "aws_route_table_association" "route_table_association3" {
   route_table_id = aws_route_table.route_table_tf.id
 } # end resource
 
+# resource "aws_security_group" "app_sec_group" {
+#   name        = "app_security_group"
+#   description = "Application Security Group"
+#   vpc_id      = aws_vpc.vpc_tf.id
+
+#   ingress {
+#     description = "TLS from VPC"
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = [aws_vpc.main.cidr_block]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   tags = {
+#     Name = "allow_tls"
+#   }
+# }
+
+
+resource "aws_security_group" "application_security_group" {
+  name        = "application_security_group"
+  description = "Application Security Group"
+  vpc_id      = aws_vpc.vpc_tf.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+
+  }
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "NodeJs"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "application_security_group"
+  }
+}
+
+resource "aws_security_group" "db_security_group" {
+  name        = "db_security_group"
+  description = "DB Security Group"
+  vpc_id      = aws_vpc.vpc_tf.id
+
+  ingress {
+    description     = "MySQL - From App Security Group"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.application_security_group.id]
+  }
+
+
+  tags = {
+    Name = "DB Security Group"
+  }
+}
+
+
+resource "aws_s3_bucket" "csye_6225_s3_bucket" {
+  bucket        = "webapp.sanket.pimple"
+  acl           = "private"
+  force_destroy = true
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+
+  }
+
+  lifecycle_rule {
+    enabled = true
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA" 
+    }
+  }
+
+  tags = {
+    Name        = "csye-6225-s3 bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_access_block" {
+  bucket = aws_s3_bucket.csye_6225_s3_bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "csye6225-db-subnet-grp"
+  subnet_ids = [aws_subnet.subnet_tf_1.id, aws_subnet.subnet_tf_2.id,aws_subnet.subnet_tf_3.id]
+
+  tags = {
+    Name = "csye6225 DB subnet group"
+  }
+}
+resource "aws_db_instance" "rds_db_instance" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "csye6225"
+  username             = "csye6225fall2020"
+  password             = "Cloud123#"
+  multi_az = false
+  identifier = "csye6225-f20"
+  vpc_security_group_ids = [aws_security_group.db_security_group.id]
+  db_subnet_group_name = "csye6225-db-subnet-grp"
+  publicly_accessible = false
+  skip_final_snapshot = true
+}
+
+
 
 # If there's connection issue, try connecting the gateway by specifying the gateway_id in association, instead of subnet id for each subnet
 
@@ -151,4 +313,22 @@ resource "aws_route_table_association" "route_table_association3" {
 #       volume_type = "gp2"
 #       volume_size = 8
 #   }
+# }
+
+
+# resource "aws_instance" "ec2instance-assignment" {
+#   ami = "ami-0e0a4a3a233572ff2"
+#   instance_type = "t2.micro"
+#   key_name = var.ssh_key_name
+#   subnet_id = aws_subnet.subnet_1_assignment.id
+#   vpc_security_group_ids = [aws_security_group.application_security_group.id]
+#   associate_public_ip_address = true
+#   root_block_device {
+#       volume_type = "gp2"
+#       volume_size = 8
+#   }
+# tags = {
+#     Name: "ec2instance-assignment"
+# }
+
 # }
